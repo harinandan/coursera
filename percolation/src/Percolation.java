376d21e03b8443af9f11ab7f343afe143e7d5afd
harinandan <harinandan@gmail.com>
2013-02-22 01:57:07 +0530

    Final solution

--- percolation/src/Percolation.java
+++ percolation/src/Percolation.java
@@ -4,17 +4,11 @@
  *
  */
 public class Percolation {
-    private class Node {
-        int data = -1;
-        Node next = null;
-    }
-
-    private WeightedQuickUnionUF algorithm;
+    private WeightedQuickUnionUF percolates;
+    private WeightedQuickUnionUF full;
     private int N;
     private int NSQUARE;
     private boolean [][]grid;
-    private Node first = null;
-    private Node last = null;
 
     /** create N-by-N grid, with all sites blocked.
      *
@@ -24,7 +18,8 @@ public class Percolation {
         this.N = N;
         NSQUARE = N * N;
         grid = new boolean[N][N];
-        algorithm = new WeightedQuickUnionUF(N * N + 2);
+        percolates = new WeightedQuickUnionUF(N * N + 2);
+        full = new WeightedQuickUnionUF(N * N + 2);
         /*
         for (int q = 1; q <= N; q++) {
             algorithm.union(0, q);
@@ -34,41 +29,6 @@ public class Percolation {
             algorithm.union(p, nSquare + 1);
         }
         */
-    }
-
-    private void add(int d) {
-        if (first == null) {
-            first = new Node();
-            last = first;
-            first.data = d;
-        } else {
-            last.next = new Node();
-            last.next.data = d;
-            last = last.next;
-        }
-    }
-
-    private void process() {
-        Node prev = null;
-        Node ret = first;
-        while (ret != null) {
-            if (algorithm.connected(0, ret.data)) {
-                algorithm.union(ret.data, NSQUARE + 1);
-                if (prev == null) {
-                    first = ret.next;
-                    if (first == null) {
-                        last = null;
-                    }
-                } else {
-                    prev.next = ret.next;
-                    if (ret == last) {
-                        last = prev;
-                    }
-                }
-            }
-            ret = ret.next;
-            prev = ret;
-        }
     }
 
     private boolean validIndex(int k) {
@@ -109,14 +69,11 @@ public class Percolation {
         grid[row][col] = true;
         int x = toOneDimensionIndex(row, col);
         if (x >= 1 && x <= N) {
-            algorithm.union(0, x);
+            percolates.union(0, x);
+            full.union(0, x);
         }
         if (x >= NSQUARE - N + 1 && x <= NSQUARE) {
-            if (algorithm.connected(0, x)) {
-                algorithm.union(x, NSQUARE + 1);
-            } else {
-                add(x);
-            }
+            percolates.union(x, NSQUARE + 1);
         }
 
         int p = toOneDimensionIndex(row, col);
@@ -129,7 +86,8 @@ public class Percolation {
             k--;
             if (validIndex(k) && isOpen(k + 1, l + 1)) {
                 q = toOneDimensionIndex(k, l);
-                algorithm.union(p, q);
+                percolates.union(p, q);
+                full.union(p, q);
             }
             k++;
 
@@ -137,7 +95,8 @@ public class Percolation {
             k++;
             if (validIndex(k) && isOpen(k + 1, l + 1)) {
                 q = toOneDimensionIndex(k, l);
-                algorithm.union(p, q);
+                percolates.union(p, q);
+                full.union(p, q);
             }
             k--;
         }
@@ -147,7 +106,8 @@ public class Percolation {
             l--;
             if (validIndex(l) && isOpen(k + 1, l + 1)) {
                 q = toOneDimensionIndex(k, l);
-                algorithm.union(p, q);
+                percolates.union(p, q);
+                full.union(p, q);
             }
             l++;
 
@@ -155,12 +115,12 @@ public class Percolation {
             l++;
             if (validIndex(l) && isOpen(k + 1, l + 1)) {
                 q = toOneDimensionIndex(k, l);
-                algorithm.union(p, q);
+                percolates.union(p, q);
+                full.union(p, q);
             }
             l--;
         }
 
-        process();
     }
 
     /** is site (row i, column j) open?
@@ -188,7 +148,7 @@ public class Percolation {
         validateSite(row, col);
 
         return isOpen(i, j)
-            && algorithm.connected(0, toOneDimensionIndex(row, col));
+            && full.connected(0, toOneDimensionIndex(row, col));
     }
 
     /** does the system percolate?
@@ -196,6 +156,6 @@ public class Percolation {
      * @return
      */
     public boolean percolates() {
-        return algorithm.connected(0, NSQUARE + 1);
+        return percolates.connected(0, NSQUARE + 1);
     }
 }
